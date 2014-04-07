2c7aec92b51799d67126f33e6d00768e1ea69178
Robert Allen <zircote@gmail.com>
2014-04-07 17:33:49 -0500

    additions for cli support

--- marathon/__init__.py
+++ marathon/__init__.py
@@ -18,28 +18,18 @@ Event Subscriptions
     GET /v2/eventSubscriptions: List all event subscriber callback URLs
     DELETE /v2/eventSubscriptions?callbackUrl={url} Unregister a callback URL from the event subscribers list
 """
-__author__ = 'allenr1'
-
 import httplib2
-from urllib import urlencode
 import json
+import urllib
+import logging
+import argparse
 
-class App(object):
-    cmd = None
-    constraints = []
-    container = {},
-    cpus = 0
-    env = {}
-    executor = None
-    id = None
-    instances = 0
-    mem = 0
-    ports = []
-    taskRateLimit = 1.0
-    tasksRunning = 3
-    tasksStaged = 0
-    uris = []
-    version = None
+
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+stream = logging.StreamHandler()
+stream.setFormatter(logFormatter)
+logging.getLogger().addHandler(stream)
+logger = logging.getLogger('py_marathon')
 
 
 class Client(object):
@@ -47,8 +37,9 @@ class Client(object):
     endpoint = None
     username = None
     password = None
-    def __init__(self, endpoint="http://localhost:8080", username=None, password=None):
-        self.endpoint = endpoint
+
+    def __init__(self, endpoint, username=None, password=None):
+        self.endpoint = endpoint.rstrip('/')
 
     @property
     def client(self):
@@ -56,9 +47,10 @@ class Client(object):
             self._client = httplib2.Http()
         return self._client
 
-    def _make_request(self, resource, method="GET"):
-        resource = "%s/%s" % (self.endpoint, resource)
-        return self.client.request(resource, method=method)
+    def _make_request(self, resource, method="GET", body=None):
+        resource = "%s%s" % (self.endpoint, resource)
+        response, content = self.client.request(resource, method=method, body=body)
+        return response, content
 
     def create_app(self, app_id, cmd, cpus=1, mem=256, instances=1, executor=None, env=None, ports=None,
                    uris=None, constraints=None, **kwargs):
@@ -82,15 +74,26 @@ class Client(object):
             if constraints is not list:
                 raise TypeError(message="argument 'constraints' must be a list", args=kwargs)
             body['constraints'] = env
-        response, content = self.client.request("%s/v2/apps", method='POST', body=json.dumps(body))
-        print response, content
+        response, content = self._make_request("/v2/apps", method='POST', body=json.dumps(body))
+        logger.debug(response)
+        logger.debug(content)
         return content
 
-    def get_apps(self, command=None, app_id=None):
-        response, content = self.client.request("%s/v2/apps" % self.endpoint)
-        if int(response.get('status')) == 200:
-            if response.get('content-type') == 'application/json':
-                content = json.loads(content)
+    def get_apps(self, cmd=None, app_id=None):
+        vars = {}
+        if cmd is not None:
+            vars['cmd'] = cmd
+        if app_id is not None:
+            vars['id'] = app_id
+        resource = "/v2/apps"
+        if len(vars) > 1:
+            vars = urllib.urlencode(vars)
+            resource = "%s?%s" % (resource, vars)
+        response, content = self._make_request(resource)
+        logger.debug(response)
+        logger.debug(content)
+        if response.get('content-type') == 'application/json':
+            content = json.loads(content)
         return content
 
     def get_app_versions(self, app_id=None, version=None):
@@ -117,9 +120,27 @@ class Client(object):
 
 class Cli(object):
 
-    def main(self):
-        pass
+    def main(self, endpoint, username, password):
+        c = Client(endpoint=endpoint, username=username, password=password)
+        return c.get_apps()
 
 
 if __name__ == '__main__':
-    pass
+    epilog = '''
+    '''
+    argparser = argparse.ArgumentParser(description='A simple AWS CloudTrail/Logstash SQS driven event loader',
+                                        epilog=epilog)
+
+    argparser.add_argument('--endpoint', '-E', type=str, help='the Marathon server endpoint', default='http://localhost:8080')
+    argparser.add_argument('--username', '-U', type=str, help='the Marathon auth username', default=None)
+    argparser.add_argument('--password', '-P', type=str, help='the Marathon auth password', default=None)
+    argparser.add_argument('--debug', action='store_true', default=False)
+    opts = argparser.parse_args()
+    if opts.debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    logger.info('py_marathon starting')
+    logger.debug("startup options: %s" % opts.__dict__)
+
+    Cli().main(opts.endpoint, opts.username, opts.password)
